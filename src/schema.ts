4463b23a84fc842d931707571f1262fb2e373d03
Sujin Jeon <sjjeon@enki.co.kr>
2024-03-25 14:46:52 +0900

    Feat: Apollo server setting with Express

---
+++ src/schema.ts
@@ -0,0 +1,15 @@
+// GraphQL schema
+const typeDefs = ` #graphql
+    type Query {
+        hello: String
+    }
+`;
+
+// A map of functions which return data for the schema
+const resolvers = {
+    Query: {
+        hello: () => 'world',
+    }
+}
+
+export { typeDefs, resolvers };
