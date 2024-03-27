2e4b81f30822d5702a5aac33f0399afaca0ab108
Sujin Jeon <sjjeon@enki.co.kr>
2024-03-27 11:53:39 +0900

    Comment: 필요없는 주석 삭제

--- src/opensearch.ts
+++ src/opensearch.ts
@@ -1,17 +1,13 @@
 import { Client } from '@opensearch-project/opensearch';
-// import fs from 'fs';
 
 const host = process.env.HOST;
 const protocol = process.env.PROTOCOL;
 const port = process.env.OPENSEARCH_PORT;
 const auth = process.env.AUTH;
 
-
-// Create a client with SSL/TLS enabled.
-console.log(protocol + '://' + auth + '@' + host + ':' + port);
 const client = new Client({
   node: protocol + '://' + auth + '@' + host + ':' + port,
-  ssl: {
+  ssl: { // Turn off the certificate verification
     rejectUnauthorized: false,
   },
 });
